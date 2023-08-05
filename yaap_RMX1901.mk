1bf5da96712fb9addba25e0c2ae33c2db7bd8d00
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-08-05 08:41:48 +0530

    Yaap

--- yaap_RMX1901.mk
+++ yaap_RMX1901.mk
@@ -10,20 +10,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
 # Inherit some common Rising stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
-# maintainer flag (one word only/no spacing)
-RISING_MAINTAINER := Shravan
 
-# chipset flag (one word only/no spacing)
-RISING_CHIPSET := sdm710
-
-# gapps build flag, if not defined build type is vanilla
-WITH_GMS := true
-TARGET_CORE_GMS := false
 
 # Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
-SUSHI_BOOTANIMATION := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # disable/enable blur support, default is false
 TARGET_ENABLE_BLUR := true
@@ -53,7 +45,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := lineage_RMX1901
+PRODUCT_NAME := yaap_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
