dfcc1ecb6678e5815c53681a8476f480a7d7aa84
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-08-06 12:13:08 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1901.mk
+    $(LOCAL_DIR)/pixys_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_RMX1901-userdebug \
-    aosp_RMX1901-eng
+    pixys_RMX1901-userdebug \
+    pixys_RMX1901-eng
 
