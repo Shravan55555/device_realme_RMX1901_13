0335226968f60a7d87fbdc56a10e2452b4704205
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-08-20 21:23:26 +0530

    Update voltage_RMX1901.mk

--- voltage_RMX1901.mk
+++ voltage_RMX1901.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Rising stuff.
 $(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 2160
 TARGET_SUPPORTS_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USE_PIXEL_CHARGER := true
